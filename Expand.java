a7b4c1e37b74dcc8f7eb155acccf7951d329e279
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-27 21:50:39 +0100

    new Expand

--- Expand.java
+++ Expand.java
@@ -1,50 +1,36 @@
-
 import java.util.Scanner;
 
 public class Expand {  
-	
 	static String input;
 	static char b;
-	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
-		System.out.println("Enter String/Text to be Expanded");
+		System.out.println("Enter String");
 		input = scan.nextLine();
-		char in = Expand(input);
-		System.out.println(in);
+		char a = Expandstring(input);
+		System.out.println(a);
 	}	
-
-	public static char Expand(String s){
-
-	//NumberFormatException
-
-	String a=null;
+	
+	public static char Expandstring(String s){
+	String a="0";
 	for(int i=0;i<input.length();i++){
 		b=input.charAt(i);
 		if(Character.isDigit(b)){
 			a=a+b;
         }
-		
 		else{
-			
         	int n=Integer.parseInt(a);
         	if(n==0){
         		System.out.print(b);
-        		
-        	}
-        	
-        	else
-        		
+        		}
+        	else{
         		for(int j=0;j<n;j++){
         		System.out.print(b);
-        		}
-        	      	
+        	    }
+        	}       	
         	a="0";
         }
 	}
 	return b;
 	} 
-
-	}
-
+}
